24143b44682a4ff8e40ec736312ad4955ee2a037
kevimota <54249894+kevimota@users.noreply.github.com>
2023-08-11 20:33:03 +0200

    for N1470 familly

--- CAENHV.hpp
+++ CAENHV.hpp
@@ -18,18 +18,19 @@ class CAEN {
         std::string address_;
         
         int sysHndl_ = -1;
-        int lk = LINKTYPE_TCPIP;
+        int lk = LINKTYPE_USB_VCP;
+        //int lk = LINKTYPE_TCPIP;
         /* int CAEN_CONN = 0;
         int MAX_ATTEMPTS = 3;
         int ATTEMPTS = 0;
         int ATTEMPTS_CONN = 1; */
         
     public:
-        CAEN(std::string name, std::string username, std::string passwd, std::string address) {
+        CAEN(std::string name, std::string commport, std::string baudrate, std::string commdata, std::string commstop, std::string commparity, std::string lbusaddress) {
             name_ = name;
-            username_ = username;
-            passwd_ = passwd;
-            address_ = address;
+            username_ = "";
+            passwd_ = "";
+            address_ = commport + "_" + baudrate + "_" + commdata + "_" + commstop + "_" + commparity + "_" + lbusaddress;
         };
         bool connect();
         void disconnect();
@@ -40,26 +41,26 @@ class CAEN {
         int getSysHndl() {
             return sysHndl_;
         };
-        float getI0(int ch, int slot) { 
-            return getfloatvalue("I0Set", ch, slot); 
+        float getISet(int ch, int slot) { 
+            return getfloatvalue("ISet", ch, slot); 
         };
-        bool setI0(int ch, int slot, float value) {
-            return setfloatvalue("I0Set", ch, slot, value);
+        bool setISet(int ch, int slot, float value) {
+            return setfloatvalue("ISet", ch, slot, value);
         };
-        float getV0(int ch, int slot) { 
-            return getfloatvalue("V0Set", ch, slot); 
+        float getVSet(int ch, int slot) { 
+            return getfloatvalue("VSet", ch, slot); 
         };
-        bool setV0(int ch, int slot, float value) {
-            return setfloatvalue("V0Set", ch, slot, value);
+        bool setVSet(int ch, int slot, float value) {
+            return setfloatvalue("VSet", ch, slot, value);
         };
         float getVMon(int ch, int slot) {
             return getfloatvalue("VMon", ch, slot);
         };
         float getIMon(int ch, int slot) {
-            return getfloatvalue("IMon", ch, slot);
+            return getfloatvalue("IMonH", ch, slot);
         };
         float getStatus(int ch, int slot) {
-            return getintvalue("Status", ch, slot);
+            return getintvalue("ChStatus", ch, slot);
         };
         bool powerOn(int ch, int slot) {
             return setintvalue("Pw", ch, slot, 1);
@@ -78,7 +79,7 @@ const char *doc_init = R"(Creates the CAEN obj.
 PYBIND11_MODULE(CAENHV, m) {
     m.doc() = "Module to connect to CAEN HV boards";
     py::class_<CAEN>(m, "CAEN")
-        .def(py::init<std::string, std::string, std::string, std::string>(), doc_init)
+        .def(py::init<std::string, std::string, std::string, std::string, std::string, std::string, std::string>(), doc_init)
         .def("connect", &CAEN::connect, "Connect to the mainframe")
         .def("disconnect", &CAEN::disconnect, "Disconnect from the mainframe")
         .def("setintvalue", &CAEN::setintvalue, "Set a int value on parameter 'param' of the 'slot.ch' of the mainframe")
@@ -86,10 +87,10 @@ PYBIND11_MODULE(CAENHV, m) {
         .def("getintvalue", &CAEN::getintvalue, "Get a int value on parameter 'param' of the board on 'slot.ch' of the mainframe")
         .def("getfloatvalue", &CAEN::getfloatvalue, "Get float value on parameter 'param' of the the board on 'slot.ch' of the mainframe")
         .def("getSysHndl", &CAEN::getSysHndl, "Returns the handler of the connection (for debug purposes)")
-        .def("getI0", &CAEN::getI0, "Get the I0Set parameter of the board on 'slot.ch' of the mainframe")
-        .def("setI0", &CAEN::setI0, "Set value on I0Set parameter of the board on 'slot.ch' of the mainframe")
-        .def("getV0", &CAEN::getV0, "Get the V0Set parameter of the board on 'slot.ch' of the mainframe")
-        .def("setV0", &CAEN::setV0, "Set value on V0Set parameter of the board on 'slot.ch' of the mainframe")
+        .def("getISet", &CAEN::getISet, "Get the I0Set parameter of the board on 'slot.ch' of the mainframe")
+        .def("setISet", &CAEN::setISet, "Set value on I0Set parameter of the board on 'slot.ch' of the mainframe")
+        .def("getVSet", &CAEN::getVSet, "Get the V0Set parameter of the board on 'slot.ch' of the mainframe")
+        .def("setVSet", &CAEN::setVSet, "Set value on V0Set parameter of the board on 'slot.ch' of the mainframe")
         .def("getVMon", &CAEN::getVMon, "Get the VMon parameter of the board on 'slot.ch' of the mainframe")
         .def("getIMon", &CAEN::getIMon, "Get the IMon parameter of the board on 'slot.ch' of the mainframe")
         .def("getStatus", &CAEN::getStatus, "Get the Status parameter of the board on 'slot.ch' of the mainframe")
